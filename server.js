a9c95c46fc7feccb3b07fa47613ddbeaa95e8bb2
Jeet Jain <Jeet88833@gmail.com>
2018-03-24 16:39:25 +0530

    Updated port address

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.post('/', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('listening on port 3000!')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
 })
